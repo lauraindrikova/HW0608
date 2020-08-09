7089fc0abd4d710e6b3207ea8708019ccaef73f2
Laura Indrikova <laura.indrikova@gmail.com>
2020-08-09 22:27:54 +0300

    Home Work 06.08

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "HW0608"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/FoodCalculator.scala
@@ -0,0 +1,7 @@
+object FoodCalculator extends App{
+val food = "potatoes"
+  val price = 0.37
+  val quantity = 20
+  val cost = price * quantity
+println(s"It will cost $cost to by $quantity kilos of $food")
+}
